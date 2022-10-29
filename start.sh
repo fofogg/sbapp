9b430f59ed99833cd4785c41402ade4b2293c64a
zong pan <panzongniub@gmail.com>
2022-10-29 10:36:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,29 @@
+export PORT=${PORT-8080}
+export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+
 echo '
-{
+ {
   "log": {
-    "loglevel": "debug"
+    "loglevel": "none"
   },
-  "inbounds": [
-    {
-      "listen": "0.0.0.0",
-      "port": 80,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "b831381d-6324-4d53-ad4f-8cda48b30811",
-            "alterId": 0
-          }
-        ]
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    }
-  ]
+    "inbounds": [
+        {
+            "type": "vmess",
+            "listen": "::",
+            "listen_port": '$PORT',
+            "users": [
+                {
+                    "uuid": "'$UUID'"
+                    "alterId": 0
+                }
+            ],
+            "transport": {
+                "type": "ws",
+                "path": "/vmess",
+                "early_data_header_name": "Sec-WebSocket-Protocol"
+            }
+        }
+    ]
 }
 ' > config.json
 
